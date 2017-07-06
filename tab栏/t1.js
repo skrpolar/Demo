e8cde1da8c2d61d4ddcb6631d7568e478ccdafb4
Guohong Liu <shokorapolar@yahoo.com>
2017-07-06 15:46:53 +0800

    v1.0

--- tab栏/t1.js
+++ tab栏/t1.js
@@ -1,37 +1,29 @@
-var head = document.getElementsByClassName("head");
-var ct6 = document.getElementById("content6");
-var ct7 = document.getElementById("content7");
-var ct8 = document.getElementById("content8");
-var ct9 = document.getElementById("content9");
-var ct10 = document.getElementById("content10");
-head[0].addEventListener("mouseover",function(){
-    ct6.style.display = "block";
-    head[0].style.opicaty
-  },false);
-head[0].addEventListener("mouseout",function(){
-    ct6.style.display = "none";
-  },false);
-head[1].addEventListener("mouseover",function(){
-    ct7.style.display = "block";
-  },false);
-head[1].addEventListener("mouseout",function(){
-    ct7.style.display = "none";
-  },false);
-head[2].addEventListener("mouseover",function(){
-    ct8.style.display = "block";
-  },false);
-head[2].addEventListener("mouseout",function(){
-    ct8.style.display = "none";
-  },false);
-head[3].addEventListener("mouseover",function(){
-    ct9.style.display = "block";
-  },false);
-head[3].addEventListener("mouseout",function(){
-    ct9.style.display = "none";
-  },false);
-head[4].addEventListener("mouseover",function(){
-    ct10.style.display = "block";
-  },false);
-head[4].addEventListener("mouseout",function(){
-    ct10.style.display = "none";
-  },false);
+var head = document.getElementsByClassName('head'),
+    margin = document.getElementById('margin');
+
+for(let j = 0; j < 5; j ++) {
+  window['ct' + (j + 6)] = document.getElementById('content' + (j + 6));
+}
+
+for(var i = 0; i < 5; i ++) {
+  ((i) => {
+    head[i].addEventListener('mouseout',() => {
+      window['ct' + (i + 6)].style.display = 'none';
+      head[i].style.backgroundColor = 'rgb(241, 255, 230)';
+      head[i].style.borderBottom = '1px solid black';
+      margin.style.backgroundColor = 'rgba(203, 196, 191, 0.4)';
+    },false);
+
+    head[i].addEventListener("mouseover",function(){
+      window['ct' + (i + 6)].style.display = 'block';
+      head[i].style.borderBottom = 'none';
+      switch(i) {
+        case 0: head[i].style.backgroundColor = 'rgb(230, 138, 157)'; margin.style.backgroundColor = 'rgb(230, 138, 157)'; break;
+        case 1: head[i].style.backgroundColor = 'rgb(138, 179, 230)'; margin.style.backgroundColor = 'rgb(138, 179, 230)'; break;
+        case 2: head[i].style.backgroundColor = 'rgb(138, 230, 166)'; margin.style.backgroundColor = 'rgb(138, 230, 166)'; break;
+        case 3: head[i].style.backgroundColor = 'rgb(230, 223, 138)'; margin.style.backgroundColor = 'rgb(230, 223, 138)'; break;
+        case 4: head[i].style.backgroundColor = 'rgb(131, 83, 91)'; margin.style.backgroundColor = 'rgb(131, 83, 91)'; break;
+      }
+    },false);
+  })(i);
+}
