61689ceea775d6ee47a29fd4f90c7aa9610defcf
skrpolar <shokorapolar@yahoo.com>
2018-06-14 11:42:19 +0800

    add System Info and Online Number

--- NetChat/assets/Script/Chat.js
+++ NetChat/assets/Script/Chat.js
@@ -3,7 +3,8 @@ cc.Class({
 
     properties: {
         m_userInput: cc.Node,
-        m_userInfomation: cc.Node
+        m_userInfomation: cc.Node,
+        m_onlineNum: cc.Node
     },
 
     userName: null,
@@ -12,26 +13,42 @@ cc.Class({
     onLoad () {
         this.socket = io.connect('localhost:5000')
         this.socket.on('userInfo', req => {
-            this.userName = req
+            this.userName = req.userName
+            let onlineNum = this.m_onlineNum.getComponent(cc.Label)
+            onlineNum.string = req.onlineNum
+            this.refreshMessage(`Welcome to NetChat! Your Name is "${this.userName}".`)
+        })
+
+        this.socket.on('DATA', req => {
+            let onlineNum = this.m_onlineNum.getComponent(cc.Label)
+            onlineNum.string = req.onlineNum
+            if (req.userName !== this.userName) {
+                this.refreshMessage(req.message)
+            }
         })
 
         this.socket.on('serverSend', req => {
-            if (req.userName !== this.userName) {
-                let newChat = new cc.Node()
-                let chatNode = newChat.addComponent(cc.Label)
-                let {userName, userInput} = req
-                chatNode.string = `${userName} say: ${userInput}`
-                newChat.parent = this.m_userInfomation
-                let iNode = this.m_userInfomation
-
-                newChat.setPosition
-                (
-                    -(iNode.width/2 - newChat.width/2),
-                    iNode.height/2 - newChat.height/2 - 50 * iNode.children.length
-                )
+            let {userName, userInput, type} = req
+            console.log(req)
+            if (userName !== this.userName) {
+                this.refreshMessage(`${userName} say: ${userInput}`)
             }
         })
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    },
+
+    refreshMessage(str) {
+        let newChat = new cc.Node()
+        let chatNode = newChat.addComponent(cc.Label)
+        chatNode.string = str
+        newChat.parent = this.m_userInfomation
+        let iNode = this.m_userInfomation
+
+        newChat.setPosition
+        (
+            -(iNode.width/2 - newChat.width/2 - 50),
+            iNode.height/2 - newChat.height/2 - 50 * iNode.children.length
+        )
     },
 
     onKeyDown(evt) {
@@ -47,7 +64,7 @@ cc.Class({
 
                 newChat.setPosition
                 (
-                    -(iNode.width/2 - newChat.width/2),
+                    -(iNode.width/2 - newChat.width/2 - 50),
                     iNode.height/2 - newChat.height/2 - 50 * iNode.children.length
                 )
 
--- socket.io服务器/socket.js
+++ socket.io服务器/socket.js
@@ -14,19 +14,44 @@ let people = []
 
 let left = []
 
-for(let i = 0; i < 30; i ++) {
+let onlineNum = 0
+
+for(let i = 1; i < 30; i ++) {
     people.push(i)
 }
 
 io.on('connection', socket => {
-    console.log('someone connected')
-    left.push(people.shift())
-    socket.emit('userInfo', 'No.' + people[0])
+    onlineNum ++
+
+    socket.name = 'No.' + people[0] // 获得当前可用最靠前名字
+    socket.emit('userInfo', {
+        userName: 'No.' + people[0],
+        onlineNum
+    })
+
+    people.shift()
+
+    socket.broadcast.emit('DATA', {
+      userName: socket.name,
+      message: `System Info: ${socket.name} coming`,
+      onlineNum
+    })
+
+    console.log(`${socket.name} connected`)
     // io.emit('serverSend', `No.${people[0]} coming`)
 
-    socket.on('disconnect', socket => {
-        people.push(left.shift())
-        console.log('someone disconnected')
+    socket.on('disconnect', () => {
+        onlineNum --
+        people.push(socket.name.substring(3, socket.name.length))
+        people.sort((x, y) => { // 返还名字资源并排序
+            return x - y
+        })
+        socket.broadcast.emit('DATA', {
+            userName: socket.name,
+            message: `System Info: ${socket.name} is leaving`,
+            onlineNum
+        })
+        console.log(`${socket.name} disconnected`)
     })
 
     socket.on('broadcast', req => {
